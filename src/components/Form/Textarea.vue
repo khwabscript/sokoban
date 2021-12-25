fe94d713b53dc20b9cc30d7d734c3c8c1ca07246
khwabscript <59730462+khwabscript@users.noreply.github.com>
2021-12-25 20:55:27 +0300

    feat: add create sokoban page

---
+++ src/components/Form/Textarea.vue
@@ -0,0 +1,36 @@
+<template>
+  <textarea class="textarea" v-model="scheme" :placeholder="placeholder"></textarea>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      scheme: '',
+      placeholder: 
+`Draw your sokoban
+
+-++++-
+++ *++
++ 0 #+
++  P++
++++++-`
+    }
+  }
+}
+</script>
+
+<style type="text/css">
+  .textarea {
+    text-align: center;
+    letter-spacing: .5rem;
+    font-size: 1rem;
+    padding: 6px;
+    margin: 1rem 0;
+    min-height: 10rem;
+    border-radius: .5rem;
+  }
+  .textarea:focus {
+    border: 2px solid black;
+  }
+</style>
---
+++ src/components/Keys.vue
@@ -0,0 +1,27 @@
+<template>
+  <table class="table" cellpadding="5" >
+    <caption class="caption">Scheme keys</caption>
+    <tbody>
+      <tr v-for="(description, key) in keys">
+        <th>{{ key.replace(' ', '”&nbsp;”') }}</th>
+        <th>{{ '&nbsp;-&nbsp;' }}</th>
+        <td style="text-align: left">{{ description }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    keys: Object
+  }
+}
+</script>
+
+<style type="text/css">
+  .caption {
+    padding: 6px;
+    font-weight: bold;
+  }
+</style>
